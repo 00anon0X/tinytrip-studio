dae515486a00d53d7d4865bc69065ffdbc8e4315
Jonathan <jonathan@users.noreply.github.com>
2026-06-01 01:50:47 +0000

    Fix GitHub Pages asset base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tinytrip-studio/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
